54a48eae01f0a57de2794b3912584848a4352aa9
chad <ccrsailors@gmail.com>
2022-07-23 15:03:50 -0500

    Created Database and Migrated Table Data

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -68,15 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'heroes_villains_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -119,3 +110,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_villains_project.local_settings import *
+except ImportError:
+    pass
