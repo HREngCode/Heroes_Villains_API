5b3d8849eff20929e124e8313b69b7fdc2250a9d
chad <ccrsailors@gmail.com>
2022-07-25 17:42:33 -0500

    Create GET and POST Endpoints for Supers

--- super/views.py
+++ super/views.py
@@ -1,12 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SupersSerializer
 from .models import Super
 
 @api_view(['GET'])
 def super_list(request):
-    super = Super.objects.all()
+    
+    if request.method == 'GET':
+        super = Super.objects.all()
+        serializer = SupersSerializer(super, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    serializer = SupersSerializer(super, many=True)
-
-    return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
